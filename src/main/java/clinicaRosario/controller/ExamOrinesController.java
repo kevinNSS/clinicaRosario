e5de048c917d7e8c18b8f79627d2b3d6f5cd8a26
Kevin Santos <kevinson.cat27@gmail.com>
2019-11-04 18:07:28 -0600

    cambios en clases

--- src/main/java/clinicaRosario/controller/ExamOrinesController.java
+++ src/main/java/clinicaRosario/controller/ExamOrinesController.java
@@ -9,7 +9,10 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import lombok.Getter;
@@ -17,7 +20,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@Named
+@Named("examOrinesController")
 @ViewScoped
 public class ExamOrinesController implements Serializable{
     
@@ -61,5 +64,48 @@ public class ExamOrinesController implements Serializable{
     
     public List<TblPacientes> getAllPacientes(){
         return tblPacientesFacade.findAll();
+    }
+    
+    public TblOrina getExamOrinesController(java.lang.Integer id){
+        return tblOrinaFacade.find(id);
+    }
+    
+    @FacesConverter (forClass = TblOrina.class)
+    public static class ExamOrinesControllerConverter implements Converter{
+        
+        @Override
+        public Object getAsObject (FacesContext facesContext, UIComponent component, String value){
+            if(value == null || value.length() == 0){
+                return null;
+            }
+            ExamOrinesController controller = (ExamOrinesController) facesContext.getApplication().getELResolver().
+                    getValue(facesContext.getELContext(), null, "examOrinesController");
+            return controller.getExamOrinesController(getKey(value));
+        }
+        
+        java.lang.Integer getKey(String value){
+            java.lang.Integer key;
+            key = Integer.valueOf(value);
+            return key;
+        }
+        
+        String getStringKey(java.lang.Integer value){
+            StringBuilder sb = new StringBuilder();
+            sb.append(value);
+            return sb.toString();
+        }
+        
+        @Override
+        public String getAsString(FacesContext facesContext, UIComponent component, Object object){
+            if(object == null){
+                return null;
+            }
+            if(object instanceof TblOrina){
+                TblOrina o = (TblOrina) object;
+                return getStringKey(o.getIdTblOrina());
+            }else{
+                throw new IllegalArgumentException("object" + object + "is of type" + object.getClass().getName() +"; expected type: " + TblOrina.class.getName());
+            }
+        }
     }
 }
